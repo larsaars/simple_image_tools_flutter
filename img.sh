9add59357152db6af23f5c28d820b2f4a69e6355
larsaars <lurzapps@gmail.com>
2021-04-11 23:17:54 +0200

    added shellscript to use with linux#

--- img.sh
+++ img.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "$1" > ~/.local/share/simple_image_tools/.sitpath
-/home/hanna/simple_image_tools_flutter/build/linux/release/bundle/simple_image_tools
+~/simple_image_tools_flutter/build/linux/release/bundle/simple_image_tools
